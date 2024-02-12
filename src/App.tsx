c00d8b44fe61b087d6c3824a3bfbe72c30f441fa
KpryKorn <roffinisacha@gmail.com>
2024-02-12 14:55:39 +0100

    init project & clear old codebase to use React instead

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <>
+      <header>
+        <img src="assets/images/logo.png" className="logo" alt="fisheye logo" />
+        <h1>Nos photographes</h1>
+      </header>
+      <main id="main">
+        <div className="photographer_section"></div>
+      </main>
+    </>
+  );
+};
+
+export default App;
