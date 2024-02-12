c45d031a8fc05b3909b88bbcff4d7b7f6fbe343c
KpryKorn <roffinisacha@gmail.com>
2024-02-12 15:50:27 +0100

    feat: add open/close modal functions

--- src/components/photographer.tsx
+++ src/components/photographer.tsx
@@ -1,18 +1,20 @@
+import { displayModal, closeModal } from "../lib/utils";
+
 const Photographer = () => {
   return (
     <>
       <main id="main">
         <div className="photograph-header">
-          {/* onClick => open la modale */}
-          <button className="contact_button">Contactez-moi</button>
+          <button className="contact_button" onClick={displayModal}>
+            Contactez-moi
+          </button>
         </div>
       </main>
       <div id="contact_modal">
         <div className="modal">
           <header>
             <h2>Contactez-moi</h2>
-            {/* onClick => close la modale */}
-            <img src="assets/icons/close.svg" />
+            <img src="/icons/close.svg" onClick={closeModal} />
           </header>
           <form>
             <div>
