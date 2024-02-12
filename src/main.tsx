a5015ac45ff361e3d39b396bd2c96e1db0210025
KpryKorn <roffinisacha@gmail.com>
2024-02-12 16:33:34 +0100

    feat: add react router to manage routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "../src/css/index.css";
+
+const homePath = "/Front-End-Fisheye/"; // This is the base path according to Vite Config ("/")
+
+const router = createBrowserRouter([
+  {
+    path: `${homePath}`,
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
