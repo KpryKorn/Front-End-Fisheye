cba25d3c7f7e15b60118d7e1223d0d07e007207d
KpryKorn <roffinisacha@gmail.com>
2024-02-12 15:50:10 +0100

    feat: add modal utility functions

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+export const displayModal = () => {
+  const modal = document.getElementById("contact_modal");
+  modal!.style.display = "block";
+};
+
+export const closeModal = () => {
+  const modal = document.getElementById("contact_modal");
+  modal!.style.display = "none";
+};
