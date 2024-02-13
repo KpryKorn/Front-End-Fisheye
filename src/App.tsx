b470222ac8ffd17044ec16e5929e890bfe07ffc7
KpryKorn <roffinisacha@gmail.com>
2024-02-13 16:58:03 +0100

    feat: add data fetching from local json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import Header from "./components/Header";
+import { Thumbnail } from "./components/photographer-items";
+import data from "./data/photographers.json"; // fetch les données du fichier json local
 
 const App = () => {
+  console.log(data.photographers);
+
   return (
     <>
       <Header />
       <main id="main">
         <div className="photographer_section">
           <Link to={"/2"}>Salut</Link>
+          <Thumbnail photographer={data.photographers[0]} />
         </div>
       </main>
     </>
