9542923944642ea51a283ed38f8c8a95a1471244
KpryKorn <roffinisacha@gmail.com>
2024-02-13 15:20:54 +0100

    refactor : delete le composant Photographer et mettre à jour l'import dans main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "../src/css/index.css";
-import Photographer from "./components/Photographer.tsx";
+import Photographer from "./routes/Photographer.tsx";
 
 const homePath = "/Front-End-Fisheye"; // This is the base path according to Vite Config ("/")
 
