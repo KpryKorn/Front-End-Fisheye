f1e9d1f5daa10c08340e00b841ac32b0003d9153
KpryKorn <roffinisacha@gmail.com>
2024-02-15 14:15:29 +0100

    feat: add ProfileText component to display data based on photographer id and url id

--- src/routes/Photographer.tsx
+++ src/routes/Photographer.tsx
@@ -1,10 +1,26 @@
+import { Link } from "react-router-dom";
 import { displayModal, closeModal } from "../lib/utils";
+import data from "../data/photographers.json";
+import fisheyeLogo from "/images/logo.png";
+import { ProfileText } from "../components/photographer-items";
 
 const Photographer = () => {
+
+  const URLId = window.location.pathname.split("/")[1];
+
+  // comparer ID de l'URL avec ID du photographe à afficher dynamiquement
+  const photographerId = data.photographers.find(
+    (photographer) => photographer.id.toString() === URLId.toString()
+  );
+
   return (
     <>
+    <header>
+      <Link to={"/"} title="Fisheye Home Page"><img src={fisheyeLogo} className="logo" alt="fisheye logo" /></Link>
+    </header>
       <main id="main">
         <div className="photograph-header">
+          <ProfileText photographer={photographerId!} />
           <button className="contact_button" onClick={displayModal}>
             Contactez moi
           </button>
