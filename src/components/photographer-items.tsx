bd4c6c5d97dee17d2af7e65387b38c9dc2483ce6
KpryKorn <roffinisacha@gmail.com>
2024-02-29 13:02:24 +0100

    style: update heart icon and likes styling

--- src/components/photographer-items.tsx
+++ src/components/photographer-items.tsx
@@ -68,7 +68,19 @@ export const DisplayTotalLikes = ({
 
   return (
     <article className="total-likes-container">
-      <p>{totalLikes} ❤️</p>
+      <p className="likes">
+        {totalLikes}{" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="total-likes-icon"
+        >
+          <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
+        </svg>
+      </p>
       <p>{`${photographer.price}€ /jour`}</p>
     </article>
   );
